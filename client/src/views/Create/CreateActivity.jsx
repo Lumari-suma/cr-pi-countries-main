6bd358f68ae577b95f1c8066e48283a55fef6638
Lumari-suma <luz.rivase@gmail.com>
2023-11-06 00:33:22 -0300

    Actualizado

--- client/src/views/Create/CreateActivity.jsx
+++ client/src/views/Create/CreateActivity.jsx
@@ -2,7 +2,7 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-case-declarations */
-import style from './createActivity.module.css'
+ import style from './createActivity.module.css'
 import { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { postActivity, getCountries } from '../../Redux/Action/action'
@@ -13,20 +13,23 @@ const CreateActivity = () => {
  const allCountries = useSelector(state => state.countries)
   const dispatch = useDispatch()
 
-
+  // Efecto que se ejecuta al cargar el componente para obtener la lista de países.
   useEffect(() => {
     dispatch(getCountries())
   }, [dispatch])
 
+
+// Estado para almacenar los datos del formulario.
   const [state, setState] = useState({
     name: '',
     picture: '',
-    difficulty: 0,
-    duration: 0,
+    difficulty: '',
+    duration: '',
     season: '',
     countries: []
   })
 
+// Estado para almacenar las actividades locales.
   const [activities, setActivities] = useState([]); // Estado local de actividades
 
   const [error, setError] = useState({
@@ -81,35 +84,55 @@ const CreateActivity = () => {
       ...updatedErrors,
     });
   };
-   
+
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
       const newActivity = dispatch(postActivity(state));
       if (newActivity) {
-        updateActivityCreation(newActivity);
+        setActivities([...activities, newActivity]);
+        resetForm(); // Limpia el formulario
       }
     } catch (error) {
       console.error('Hubo un error al crear la actividad:', error);
     }
- 
-    const updateActivityCreation = async (newActivity) => {
-      try {
-        setActivities([...activities, newActivity]);
-        for (let property in state) {
-          document.getElementById(property).value = "";
-        }
-        /* resetForm();  */
-      } catch (error) {
-        console.error('Hubo un error al crear la actividad:', error);
-      }
-    };
   };
+  
+  const resetForm = () => {
+    setState({
+      name: '',
+      picture: '',
+      difficulty: '',
+      duration: '',
+      season: '',
+      countries: []
+    });
+    setError({
+      name: 'Nombre requerido',
+      picture: '',
+      difficulty: '',
+      duration: '',
+      season: '',
+      countries: 'Selecciona al menos un país'
+    });
+  };
+   
+   /*  for (let property in state) {
+          document.getElementById(property).value = "";
+        }*/
   
   const [handlerDisable, setHandlerDisable] = useState(true)
 
   const disableFunction = () => {
-    let disabledAux = false; // 
+    const hasErrors = Object.values(error).some((err) => err.length !== 0);
+    const hasEmptyFields = Object.values(state).some((value) => value === '');
+  
+    // Deshabilitar el botón si hay errores o campos vacíos.
+    setHandlerDisable(hasErrors || hasEmptyFields);
+  };
+
+ /*  const disableFunction = () => {
+    let disabledAux = false; 
 
     for (let err in error) {
       if (err.length) disabledAux = true
@@ -117,7 +140,7 @@ const CreateActivity = () => {
     disabledAux = Object.values(error).some((err) => err.length !== 0)
     console.log(error)
     setHandlerDisable(disabledAux)
-  } 
+  }  */
 
   return (
     
@@ -126,63 +149,93 @@ const CreateActivity = () => {
         <h2 className={style.form_title}>Crea tu nueva actividad Turistica</h2>
         <div >
           <label className={style.form_label}>Actividad: </label>
-          <input id='name' className={style.form_input} name='name' onChange={handleChange} type="text" value={state.name} placeholder="Nombre de la actividad" />
-          <label className={style.error}>{error.name}</label> {/* ok */}
+            <input className={style.form_input}
+              id='name' 
+              name='name' 
+              type="text" 
+              onChange={handleChange} 
+              value={state.name} 
+              placeholder="Nombre de la actividad" />
+          <label className={style.error}>{error.name}</label> 
         </div>
 
         <div >
           <label className={style.form_label}>Imagen: </label>
-          <input id='picture' className={style.form_input} name='picture' onChange={handleChange} type="picture" value={state.picture} placeholder='ingresa la URL de la imgen'/>
+            <input className={style.form_input}
+              id='picture'  
+              name='picture' 
+              type="text" 
+              onChange={handleChange} 
+              value={state.picture} 
+              placeholder='ingresa URL de la imgen'/>
           <label className={style.error}>{error.picture}</label>
         </div>
 
         <div>
           <label className={style.form_label}>Dificultad: </label>
-          <select className={style.form_input} name='difficulty' onChange={handleChange} id='difficulty'>
-            <option value=''>-</option>
-            <option value='1'>1</option>
-            <option value='2'>2</option>
-            <option value='3'>3</option>
-            <option value='4'>4</option>
-            <option value='5'>5</option>
-          </select>
+           <select className={style.form_input} 
+              id='difficulty'
+              name='difficulty' 
+              onChange={handleChange} >
+                <option value=''>-</option>
+                <option value='1'>1</option>
+                <option value='2'>2</option>
+                <option value='3'>3</option>
+                <option value='4'>4</option>
+                <option value='5'>5</option>
+           </select>
         </div>
 
         <div>
           <label className={style.form_label}>Duración de la Actividad: </label>
-          <input id='durationz' className={style.form_input} name='duration' onChange={handleChange} type="text" value={state.duration} placeholder=' ' />
+           <input className={style.form_input} 
+              id='duration'  
+              name='duration' 
+              type="text" 
+              onChange={handleChange} 
+              value={state.duration} 
+              placeholder='Max. 5 horas ' />
           <label className={style.error}>{error.duration}</label>
         </div>
 
         <div>
           <label className={style.form_label}>Temporada:</label>
-          <select className={style.form_input} onChange={handleChange} name='season'>
-            <option value=''>-Selecciona temporada-</option>
-            <option value='Summer'>Verano</option>
-            <option value='Fall'>Otoño</option>
-            <option value='Winter'>Invierno</option>
-            <option value='Spring'>Primavera</option>
+           <select className={style.form_input} 
+             name='season'
+             onChange={handleChange}>
+               <option value=''>-Selecciona temporada-</option>
+               <option value='Summer'>Verano</option>
+               <option value='Fall'>Otoño</option>
+               <option value='Winter'>Invierno</option>
+               <option value='Spring'>Primavera</option>
           </select>
           <label className={style.error}>{error.season}</label>
         </div>
         <div>
           <label className={style.form_label}>Paises: </label>
-          <select className={style.form_input} onChange={handleChange} name='countries'>
+           <select className={style.form_input} 
+            name='countries'
+            onChange={handleChange}>
             <option>- Selecciona un país -</option>
             {allCountries?.map((c) => <option value={c.ID} key={c.ID}>{c.name}</option>)}
-          </select>
+           </select>
           <div className={style.form_group}>
               {
                 state.countries?.map((c, index) => (
                   <div key={index}>
                     <label>{c}</label> 
-                    <button className={style.buttonX} name='countries' id={c} type='button' onClick={handleDelete}>x</button>
+                    <button className={style.buttonX} 
+                    name='countries' 
+                    id={c} 
+                    type='button' 
+                    onClick={handleDelete}
+                    >x</button>
                   </div>
                 ))
               }
 
           </div>
-          <label className={style.error}>{error.countries}</label> 
+            <label className={style.error}>{error.countries}</label> 
         </div>
         
         {handlerDisable ? (
--- client/src/views/Create/validate.js
+++ client/src/views/Create/validate.js
@@ -1,6 +1,62 @@
 /* eslint-disable no-case-declarations */
 /* eslint-disable no-useless-escape */
-export const validate = (stateAux, name,) => {
+export const validate = (stateAux, name) => {
+  const errors = {
+    name: '',     // Inicializa los errores en blanco
+    picture: '',
+    difficulty: '',
+    duration: '',
+    season: '',
+    countries: '',
+  };
+
+  const REGEX_SYMBOL = /^[a-zA-ZñÑ\s]+$/;  // Expresión regular para letras y espacios
+  const REGEX_URL = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*/;  // Expresión regular para URL
+
+  if (name === 'name') {
+    if (!stateAux.name.length) {
+      errors.name = 'Nombre requerido';
+    } else if (!REGEX_SYMBOL.test(stateAux.name)) {
+      errors.name = 'No se permiten símbolos ni números';
+    } else if (stateAux.name.length > 30) {
+      errors.name = 'Nombre de actividad demasiado largo';
+    }
+  }
+
+  if (name === 'duration') {
+    const durationValue = parseFloat(stateAux.duration);
+    if (isNaN(durationValue) || !Number.isInteger(durationValue) || durationValue < 0) {
+      errors.duration = 'Entrada no válida. Debe ser un número entero positivo';
+    } else if (durationValue > 5) {
+      errors.duration = 'Duración máxima de la actividad: 5 horas';
+    }
+  }
+
+  if (name === 'picture') {
+    if (!REGEX_URL.test(stateAux.picture)) {
+      errors.picture = 'Se permite formato de imagen jpg, png y jpng';
+    }
+  }
+
+  if (name === 'season') {
+    if (!stateAux.season) {
+      errors.season = 'Por favor, seleccione una temporada.';
+    }
+  }
+
+  if (name === 'countries') {
+    if (stateAux.countries.length === 0) {
+      errors.countries = 'Elige al menos un país';
+    }
+  }
+
+  return errors;
+};
+
+
+
+
+/*  export const validate = (stateAux, name,) => {
   const errors = {
     name: '',
     picture: '',
@@ -61,4 +117,4 @@ export const validate = (stateAux, name,) => {
   }
 
   return errors;
-};
+};  */
